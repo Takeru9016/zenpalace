d41276fd6f1ea1d7c007372d610b53b9751cf01b
Takeru9016 <youthfox046@gmail.com>
2023-11-23 21:41:35 +0530

    feat: Completed Our Motto & Half Navbar Section

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
+import { PRODUCT_CATEGORIES } from "@/config";
+import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks";
+
+export default function NavItems() {
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+  const isAnyOpen = activeIndex !== null;
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+    document.addEventListener("keydown", handler);
+
+    return () => {
+      document.addEventListener("keydown", handler);
+    };
+  }, []);
+
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
+  return (
+    <div className="flex gap-4 h-full" ref={navRef}>
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
+
+        const isOpen = i === activeIndex;
+
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            isAnyOpen={isAnyOpen}
+            key={category.value}
+          />
+        );
+      })}
+    </div>
+  );
+}
